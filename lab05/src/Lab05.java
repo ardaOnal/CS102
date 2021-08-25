d1058850fd4444b6497881868d559518a5e40844
ardaOnal <63498645+ardaOnal@users.noreply.github.com>
2021-08-25 15:36:32 +0300

    Delete lab05 directory

--- lab05/src/Lab05.java
+++
@@ -1,9 +0,0 @@
-// ***********************************************
-// Lab05 ~ MVC GUI Hangman ~ Spring 2019/20
-// David
-// ***********************************************
-
-// Compile & Run "hangmangame.ConsoleHangmanDemo"
-
-// Compile & Run "hangmangame.GUIHangmanDemo"
-
